e7752ad431d3a6a4d16677c044031e4fce0c69eb
David Pink <dvdpink@gmail.com>
2019-11-08 20:25:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ function takeANumber (line, name) {
 line.push(name)
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
-function nowServing (currentline){
-  if(currentline.length >= 1){
-    return "Currently serving" + " " + currentline[0] + ".";
-    currentline.slice(1)
-  } else if(currentline.length < 1) {
-  return  "There is nobody waiting to be served!"
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
   }
+
+  return `Currently serving ${line.shift()}.`
 }
